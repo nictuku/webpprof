90d0ce0df11c1db9eaf85cd0ba4d82036a5d2cd8
Yves Junqueira <yves.junqueira@gmail.com>
2014-05-09 03:05:00 +0200

    cznic/ql for datastore

--- ppserver/ppserver.go
+++ ppserver/ppserver.go
@@ -1,22 +1,32 @@
 package ppserver
 
 import (
-	"appengine"
-	"appengine/datastore"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
+	// Install the QL SQL driver.
+	_ "github.com/cznic/ql/driver"
 	"github.com/nictuku/webpprof/ppcommon"
 )
 
-func init() {
-	http.HandleFunc("/profile", HandlePostProfile)
+var (
+	db   *sql.DB
+	once sync.Once
+)
 
-}
-
+// HandlePostProfile receives a pprof profile and stores it.
 func HandlePostProfile(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("ql", "ql.db")
+		if err != nil {
+			log.Fatalf("db opening error: %v", err)
+		}
+	})
 
 	p := r.FormValue("p")
 	if p == "" {
@@ -26,25 +36,47 @@ func HandlePostProfile(w http.ResponseWriter, r *http.Request) {
 	var profile ppcommon.Profile
 	defer r.Body.Close()
 	dec := json.NewDecoder(r.Body)
-	err := dec.Decode(&profile)
-	if err != nil {
+	if err := dec.Decode(&profile); err != nil {
 		log.Printf("handle post profile error parsing JSON for %v: %v", p, err)
 		http.Error(w, "invalid profile content", http.StatusBadRequest)
 		return
 	}
-
-	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.String()))
-	/*	body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("handleProfile read body error: %v", err)
-			return
-		}
-	*/
-	_, err = datastore.Put(c, datastore.NewIncompleteKey(c, "Profile", nil), &profile)
-	if err != nil {
+	if err := saveProfile(&profile); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+// Schema:
+//
+// CREATE TABLE profiles (user string, profile blob, t time);
+
+func saveProfile(p *Profile) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return
+	}
+	result, err := tx.Exec(`
+		INSERT INTO profiles VALUES ($1, $2, now());`,
+		"yves.junqueira@gmail.com", profile.Content)
+
+	if err != nil {
+		return
+	}
+	if err := tx.Commit(); err != nil {
+		return
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+	aff, err := result.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("LastInsertId %d, RowsAffected %d\n", id, aff)
+
 	log.Printf("decoded profile: %+q", profile)
 	return
 }
